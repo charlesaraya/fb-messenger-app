b5934a24a99af8847293e2ff78b7f66655d01c50
Charles Araya <charlesaraya@gmail.com>
2016-08-03 11:13:59 +0200

    Cleaned charlesaraya/jincana to create NodeJS module

---
+++ app.js
@@ -0,0 +1,493 @@
+import request from 'request'
+
+class Messenger {
+
+  constructor (token, notificationType) {
+    this.token = token
+    this.notificationType = notificationType || 'REGULAR'
+  }
+
+  /*
+   * Messange event dispatcher
+   */
+  messageDispatcher(data, cb) {
+    // Check that this is a page subscription
+    if (data.object == 'page') {
+
+      data.entry.forEach((pageEntry) => {
+        var pageId = pageEntry.id
+        var timeOfEvent = pageEntry.time
+
+        pageEntry.messaging.forEach((event) => {
+          if (event.message) {
+            receivedAuthentication(event)
+          } else if (event.optin) {
+            receivedMessage(event)
+          } else if (event.delivery) {
+            receivedDeliveryConfirmation(event)
+          } else if (event.postback) {
+            receivedPostback(event)
+          } else if (event.read) {
+            receivedReadConfirmation(event)
+          } else {
+            console.log('Webhook received an unknown messaging event: ', event)
+          }
+        })
+      })
+    }
+  }
+  
+  /*
+   * Authorization Event
+   *
+   */
+  receivedAuthentication(event) {
+    var sender = event.sender.id
+    var recipient = event.recipient.id
+    var timeOfAuth = event.timestamp
+    var passThroughParam = event.optin.ref
+
+    console.log('Authentication received for user %d and page %d with pass ' + 
+      "through param '%s' at %d", sender, recipient, passThroughParam, timeOfAuth)
+  }
+
+  /*
+   * Received Message Event
+   *
+   */
+  receivedMessage(event) {
+    var sender = event.sender.id
+    var recipient =  event.recipient.id
+    var timeOfMessage = event.timestamp
+    var message = event.message
+
+    console.log('Received Message from user %d and page %d at %d with message: ', 
+      sender, recipient, timeOfMessage, message)
+  }
+
+  /*
+   * Delivery Confirmation Event
+   *
+   */
+  receivedDeliveryConfirmation(event) {
+    var sender = event.sender.id
+    var recipient = event.recipient.id
+    var messages = event.delivery.mids
+    var watermark = event.delivery.watermark
+    var sequence = event.delivery.seq
+
+    if (messages) {
+      messages.forEach((message) => {
+        console.log('Received delivery confirmation for message ID: %s', message)
+      })
+    }
+    console.log('All messages before %d were delivered', watermark)
+  }
+
+  /*
+   * Postback Event
+   *
+   */
+  receivedPostback(event) {
+    var sender = event.sender.id
+    var recipient = event.recipient.id
+    var timeOfPostback = event.timestamp
+    var payload = event.postback.payload
+
+    console.log("Received postback for user %d and page %d with payload '%s' at %d", 
+      sender, recipient, payload, timeOfPostback)
+  }
+
+  /*
+   * Message Read Event
+   *
+   */
+  receivedReadConfirmation(event) {
+    var sender = event.sender.id
+    var recipient = event.recipient.id
+    var timeOfRead = event.timestamp
+    var watermark = event.read.watermark
+    var sequence = event.read.seq
+
+    console.log('All Messages were read from user %d and page %d at %d', sender, 
+      recipient, timeOfRead)
+  }
+
+  /*
+   * Send a plain text message
+   *
+   */
+  sendTextMessage(recipientId, text, notificationType, cb) {
+    var message = {
+      text: text
+    }
+    sendMessage(recipientId, message, notificationType, cb)
+  }
+
+  /*
+   * Send an image (jpg, png and gifs)
+   *
+   */
+  sendImageMessage(recipientId, imgUrl, notificationType, cb) {
+    var message = {
+      attachment: {
+        type: 'image',
+        payload: {
+          url: imgUrl
+        }
+      }
+    }
+    sendMessage(recipientId, message, notificationType, cb)
+  }
+
+  /*
+   * Send an audio
+   *
+   */
+  sendAudioMessage(recipientId, audioUrl, notificationType, cb) {
+    var message = {
+      attachment: {
+        type: 'audio',
+        payload: {
+          url: audioUrl
+        }
+      }
+    }
+    sendMessage(recipientId, message, notificationType, cb)
+  }
+
+  /*
+   * Send a video
+   *
+   */
+  sendVideoMessage(recipientId, videoUrl, notificationType, cb) {
+    var message = {
+      attachment: {
+        type: 'video',
+        payload: {
+          url: videoUrl
+        }
+      }
+    }
+    sendMessage(recipientId, message, notificationType, cb)
+  }
+
+  /*
+   * Send a file
+   *
+   */
+  sendFileMessage(recipientId, fileUrl, notificationType, cb) {
+    var message = {
+      attachment: {
+        type: 'video',
+        payload: {
+          url: fileUrl
+        }
+      }
+    }
+    sendMessage(recipientId, message, notificationType, cb)
+  }
+
+  /*
+   * Send a button message
+   *
+   */
+   sendButtonMessage(recipientId, buttonTemplate, notificationType, cb) {
+    var message = {
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'button',
+          text: buttonTemplate.text,
+          buttons: buttonTemplate.buttons
+        }
+      }
+    }
+    sendMessage(recipientId, message, notificationType, cb)
+   }
+
+  /*
+   * Send a Structured Message
+   *
+   */
+  sendGenericMessage(recipientId, elements, notificationType, cb) {
+    if (typeof elements != 'array') {
+      elements = [elements]
+    }
+    var message = {
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'generic',
+          elements: elements
+        }
+      }
+    }
+    sendMessage(recipientId, message, notificationType, cb)
+  }
+
+  /*
+   * Send a receipt message
+   *
+   */
+  sendReceiptMessage(recipientId, receipt, notificationType, cb) {
+    var message = {
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'receipt',
+          recipient_name: receipt.recipient_name,
+          order_number: receipt.order_number,
+          currency: receipt.currency,
+          payment_method: receipt.payment_method,
+          order_url: receipt.order_url,
+          timestamp: receipt.timestamp,
+          elements: receipt.shopping_cart,
+          address: receipt.recipient_address,
+          summary: receipt.order_summary,
+          adjustments: receipt.adjustments
+        }
+      }
+    }
+    sendMessage(recipientId, message, notificationType, cb)
+  }
+
+  /*
+   * Send a quick reply message.
+   *
+   */
+  sendQuickMessage(recipientId, quickReplies, notificationType, cb) {
+    var message = {
+      text: quickReplies.text,
+      quick_replies: quickReplies.quick_replies
+    }
+    sendMessage(recipientId, message, notificationType, cb)
+  }
+
+  /*
+   * Send an airplane itinerary message
+   *
+   */
+  sendItineraryMessage(recipientId, itinerary, notificationType, cb) {
+    var message = {
+      attachment: {
+        type: "template",
+        payload: itinerary
+      }
+    }
+    sendMessage(recipientId, message, notificationType, cb)
+  }
+
+  /*
+   * Send an airplane check-in reminder message
+   *
+   */
+  sendCheckinMessage(recipientId, checkIn, notificationType, cb) {
+    var message = {
+      attachment: {
+        type: 'template',
+        payload: checkIn
+      }
+    }
+    sendMessage(recipientId, message, notificationType, cb)
+  }
+
+  /*
+   * Send an Airplane Boarding Pass message
+   *
+   */
+  sendBoardingpassMessage(recipientId, boardingpass, notificationType, cb) {
+    var message = {
+      attachment: {
+        type: 'template',
+        payload: boardingpass
+      }
+    }
+    sendMessage(recipientId, message, notificationType, cb)
+  }
+
+  /*
+   * Send a flight update message
+   *
+   */
+  sendFlightupdateMessage(recipientId, flightupdate, notificationType, cb) {
+    var message = {
+      attachment: {
+        type: 'template',
+        payload: flightupdate
+      }
+    }
+    sendMessage(recipientId, message, notificationType, cb)
+  }
+
+  /*
+   * Send a message to a user.
+   *
+   */
+  sendMessage(recipientId, message, notificationType, cb) {
+    if (typeof notificationType === 'function') {
+      cb = notificationType
+      notificationType = NOTIFICATION_TYPE
+    }
+    const req = {
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {
+        access_token: this.token
+      },
+      method: 'POST',
+      json: {
+        recipient: {
+          id: recipientId
+        },
+        message: message,
+        notification_type: notificationType
+      }
+    }
+    sendRequest(req, cb)
+  }
+
+  /*
+   * Send Sender Actions
+   *
+   */
+  sendSenderAction(recipientId, senderAction, cb) {
+    const req = {
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {
+        access_token: this.token
+      },
+      method: 'POST',
+      json: {
+        recipient: {
+          id: recipientId
+        },
+        sender_action: senderAction
+      }
+    }
+    sendRequest(req, cb)
+  }
+
+  /*
+   * Set a greeting text
+   *
+   */
+  setGreetingText(message, cb) {
+    if (typeof message === 'string') {
+      message = {text: message}
+    }
+    var method = 'POST'
+    var params = {
+      setting_type: 'greeting',
+      greeting: message
+    }
+    sendThreadSettingsRequest(method, params, cb)
+  }
+
+  /*
+   * Set a Get Started Button
+   *
+   */
+  setGetStartedButton(message, cb) {
+    if (typeof message === 'string') {
+      message = [{
+        payload: message
+      }]
+    }
+    var method = 'POST'
+    var params =  {
+      setting_type: 'call_to_actions',
+      thread_state: 'new_thread',
+      call_to_actions : {
+        payload: message
+      }
+    }
+    sendThreadSettingsRequest(method, params, cb)
+  }
+
+  /*
+   * Delete the Get Started Button
+   *
+   */
+  deleteGetStartedButton(cb) {
+    var method = 'DELETE'
+    var params =  {
+      setting_type: 'call_to_actions',
+      thread_state: 'new_thread'
+    }
+    sendThreadSettingsRequest(method, params, cb)
+  }
+
+  /*
+   * Set a Persistent Menu
+   * 
+   */
+  setPersistentMenu(menuItems, cb) {
+    var method = 'POST'
+    var params =  {
+      setting_type: 'call_to_actions',
+      thread_state: 'existing_thread',
+      call_to_actions : menuItems
+    }
+    sendThreadSettingsRequest(method, params, cb)
+  }
+
+  /*
+   * Delete the Persistent Menu
+   *
+   */
+  deletePersistentMenu(cb) {
+    var method = 'DELETE'
+    var params = {
+      setting_type: 'call_to_actions',
+      thread_state: 'existing_thread'
+    }
+    sendThreadSettingsRequest(method, params, cb)
+  }
+
+  sendThreadSettingsRequest(method, params, cb) {
+    const req = {
+      url: 'https://graph.facebook.com/v2.6/me/thread_settings',
+      qs: {
+        access_token: this.token
+      },
+      method: method,
+      json: params
+    }
+    sendRequest(req, cb)
+  }
+
+  /*
+   * Get the User Profile
+   *
+   */
+  getUserProfile(userId, cb) {
+    const req = {
+      url: `https://graph.facebook.com/v2.6/${userId}`,
+      qs: {
+        access_token: this.token,
+        fields: 'first_name,last_name,profile_pic,locale,timezone,gender'
+      },
+      method: 'GET',
+      json: true
+    }
+    sendRequest(req, cb)
+  }
+
+  /*
+   * Send Request to API
+   *
+   */
+  const sendRequest = (req, cb) => {
+    request(req, (error, response, body) => {
+      if (!cb) 
+        return
+      if (error) 
+        return cb(error)
+      if (response.body.error) 
+        return cb(response.body.error)
+      cb(null, response.body)
+    })
+  }
+}
+
+export default Messenger
