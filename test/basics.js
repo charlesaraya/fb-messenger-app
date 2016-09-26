eb1e8740b34bae8644577cfc25f720d139532397
Charles Araya <charlesaraya@gmail.com>
2016-09-26 17:44:59 +0200

    feat(test): add sendAction test

--- test/basics.js
+++ test/basics.js
@@ -46,138 +46,156 @@ describe('Bot', function () {
     describe('#private methods', function () {
       let bot = new Messenger(dummyToken)
 
-      it('should have a getToken function', function () {
+      it('should have a getToken method', function () {
         expect(bot.getToken).to.exist;
         expect(bot.getToken).to.be.a('function');
       })
-      it('should have a getApiUrl function', function () {
+      it('should have a getApiUrl method', function () {
         expect(bot.getApiUrl).to.exist;
         expect(bot.getApiUrl).to.be.a('function');
       })
-      it('should have a getNotificationType function', function () {
+      it('should have a getNotificationType method', function () {
         expect(bot.getNotificationType).to.exist;
         expect(bot.getNotificationType).to.be.a('function');
       })
-      it('should have a sendApiMessage function', function () {
+      it('should have a sendApiMessage method', function () {
         expect(bot.sendApiMessage).to.exist;
         expect(bot.sendApiMessage).to.be.a('function');
       })
-      it('should have a subscribeApp function', function () {
+      it('should have a subscribeApp method', function () {
         expect(bot.subscribeApp).to.exist;
         expect(bot.subscribeApp).to.be.a('function');
       })
-      it('should have a getUserPsid function', function () {
+      it('should have a getUserPsid method', function () {
         expect(bot.getUserPsid).to.exist;
         expect(bot.getUserPsid).to.be.a('function');
       })
-      it('should have a unlinkAccount function', function () {
+      it('should have a unlinkAccount method', function () {
         expect(bot.unlinkAccount).to.exist;
         expect(bot.unlinkAccount).to.be.a('function');
       })
-      it('should have a getUserProfile function', function () {
+      it('should have a getUserProfile method', function () {
         expect(bot.getUserProfile).to.exist;
         expect(bot.getUserProfile).to.be.a('function');
       })
-      it('should have a _handleCallback function', function () {
+      it('should have a _handleCallback method', function () {
         expect(bot._handleCallback).to.exist;
         expect(bot._handleCallback).to.be.a('function');
       })
-      it('should have a _handleEvent function', function () {
+      it('should have a _handleEvent method', function () {
         expect(bot._handleEvent).to.exist;
         expect(bot._handleEvent).to.be.a('function');
       })
-      it('should have a threadSetting.setGreetingText function', function () {
-        expect(bot.threadSetting.setGreetingText).to.exist;
-        expect(bot.threadSetting.setGreetingText).to.be.a('function');
+      describe('#ThreadSetting', function () {
+        it('should have a setGreetingText method', function () {
+          expect(bot.threadSetting.setGreetingText).to.exist;
+          expect(bot.threadSetting.setGreetingText).to.be.a('function');
+        })
+        it('should have a setGetStartedButton method', function () {
+          expect(bot.threadSetting.setGetStartedButton).to.exist;
+          expect(bot.threadSetting.setGetStartedButton).to.be.a('function');
+        })
+        it('should have a setPersistentMenu method', function () {
+          expect(bot.threadSetting.setPersistentMenu).to.exist;
+          expect(bot.threadSetting.setPersistentMenu).to.be.a('function');
+        })
+        it('should have a deleteGetStartedButton method', function () {
+          expect(bot.threadSetting.deleteGetStartedButton).to.exist;
+          expect(bot.threadSetting.deleteGetStartedButton).to.be.a('function');
+        })
+        it('should have a deletePersistentMenu method', function () {
+          expect(bot.threadSetting.deletePersistentMenu).to.exist;
+          expect(bot.threadSetting.deletePersistentMenu).to.be.a('function');
+        })
+        it('should have a sendSettingRequest method', function () {
+          expect(bot.threadSetting.sendSettingRequest).to.exist;
+          expect(bot.threadSetting.sendSettingRequest).to.be.a('function');
+        })
       })
-      it('should have a threadSetting.setGetStartedButton function', function () {
-        expect(bot.threadSetting.setGetStartedButton).to.exist;
-        expect(bot.threadSetting.setGetStartedButton).to.be.a('function');
+      describe('#SenderAction', function () {
+        it('should have a sendMarkSeenInterval method', function () {
+          expect(bot.senderAction.sendMarkSeenInterval).to.exist;
+          expect(bot.senderAction.sendMarkSeenInterval).to.be.a('function');
+        })
+        it('should have a sendTypingInterval method', function () {
+          expect(bot.senderAction.sendTypingInterval).to.exist;
+          expect(bot.senderAction.sendTypingInterval).to.be.a('function');
+        })
+        it('should have a sendSenderActionRequest method', function () {
+          expect(bot.senderAction.sendSenderActionRequest).to.exist;
+          expect(bot.senderAction.sendSenderActionRequest).to.be.a('function');
+        })
       })
-      it('should have a threadSetting.setPersistentMenu function', function () {
-        expect(bot.threadSetting.setPersistentMenu).to.exist;
-        expect(bot.threadSetting.setPersistentMenu).to.be.a('function');
-      })
-      it('should have a threadSetting.deleteGetStartedButton function', function () {
-        expect(bot.threadSetting.deleteGetStartedButton).to.exist;
-        expect(bot.threadSetting.deleteGetStartedButton).to.be.a('function');
-      })
-      it('should have a threadSetting.deletePersistentMenu function', function () {
-        expect(bot.threadSetting.deletePersistentMenu).to.exist;
-        expect(bot.threadSetting.deletePersistentMenu).to.be.a('function');
-      })
-      it('should have a threadSetting.sendSettingRequest function', function () {
-        expect(bot.threadSetting.sendSettingRequest).to.exist;
-        expect(bot.threadSetting.sendSettingRequest).to.be.a('function');
-      })
-      it('should have a verify.webhook function', function () {
-        expect(bot.verify.webhook).to.exist;
-        expect(bot.verify.webhook).to.be.a('function');
-      })
-      it('should have a verify.signature function', function () {
-        expect(bot.verify.signature).to.exist;
-        expect(bot.verify.signature).to.be.a('function');
+      describe('#Verify', function () {
+        it('should have a webhook method', function () {
+          expect(bot.verify.webhook).to.exist;
+          expect(bot.verify.webhook).to.be.a('function');
+        })
+        it('should have a signature method', function () {
+          expect(bot.verify.signature).to.exist;
+          expect(bot.verify.signature).to.be.a('function');
+        })
       })
     })
 
     describe('#arguments', function () {
       let token = 'foo'
 
-      it('should throw an Error when token is missing', function () {
-        let badBot = function () { bot = new Messenger() }
-        expect(badBot).to.throw(Error, 'Facebook Page access token is missing.')
+      describe('#token', function () {
+        it('should throw an Error when missing', function () {
+          let badBot = function () { bot = new Messenger() }
+          expect(badBot).to.throw(Error, 'Facebook Page access token is missing.')
+        })
+        it('should be of type string', function () {
+          let bot = new Messenger(token)
+          expect(bot.getToken()).to.be.a('string')
+        })
+        it('should store token correctly', function () {
+          let bot = new Messenger(token)
+          expect(bot.getToken()).to.equal(token) 
+        })
       })
+      describe('#apiUrl', function () {
+        it('should store default if missing', function () {
+          let bot = new Messenger(token)
+          expect(bot.getApiUrl()).to.equal('https://graph.facebook.com/v2.6/')
+        })
 
-      it('should store a token of type string', function () {
-        let bot = new Messenger(token)
-
-        expect(bot.getToken()).to.be.a('string')
+        it('should store option.apiUrl correctly', function () {
+          let options = {apiUrl: 'foo'}
+          let bot = new Messenger(token, options)
+          expect(bot.getApiUrl()).to.equal('foo')
+        })
       })
+      describe('#notificationType', function () {
+        it('should store "REGULAR" as default if not passed as option', function () {
+          let bot = new Messenger(token)
+          expect(bot.getNotificationType()).to.equal('REGULAR')
+        })
 
-      it('should store "https://graph.facebook.com/v2.6/" in apiUrl in case it was not passed as option', function () {
-        let bot = new Messenger(token)
+        it('should store default "REGULAR" if option is not supported', function () {
+          let options = {notificationType: 'UNSUPPORTED_NOTIFICATION_TYPE'}
+          let bot = new Messenger(token, options)
+          expect(bot.getNotificationType()).to.equal('REGULAR')
+        })
 
-        expect(bot.getApiUrl()).to.equal('https://graph.facebook.com/v2.6/')
-      })
+        it('should store "REGULAR" if passed as an option', function () {
+          let options = {notificationType: 'REGULAR'}
+          let bot = new Messenger(token, options)
+          expect(bot.getNotificationType()).to.equal('REGULAR')
+        })
 
-      it('should store an optional apiUrl correctly', function () {
-        let options = {apiUrl: 'foo'}
-        let bot = new Messenger(token, options)
+        it('should store "SILENT_PUSH" if passed as an option', function () {
+          let options = {notificationType: 'SILENT_PUSH'}
+          let bot = new Messenger(token, options)
+          expect(bot.getNotificationType()).to.equal('SILENT_PUSH')
+        })
 
-        expect(bot.getApiUrl()).to.equal('foo')
-      })
-
-      it('should store "REGULAR" in notif.Type if no optional notif.Type was passed', function () {
-        let bot = new Messenger(token)
-
-        expect(bot.getNotificationType()).to.equal('REGULAR')
-      })
-      it('should store "REGULAR" in notif.Type if the optional notif.Type isn\'t supported', function () {
-        let options = {notificationType: 'UNSUPPORTED_NOTIFICATION_TYPE'}
-        let bot = new Messenger(token, options)
-
-        expect(bot.getNotificationType()).to.equal('REGULAR')
-      })
-
-      it('should store "REGULAR" in notificationType if passed as an option', function () {
-        let options = {notificationType: 'REGULAR'}
-        let bot = new Messenger(token, options)
-
-        expect(bot.getNotificationType()).to.equal('REGULAR')
-      })
-
-      it('should store "SILENT_PUSH" in notificationType if passed as an option', function () {
-        let options = {notificationType: 'SILENT_PUSH'}
-        let bot = new Messenger(token, options)
-
-        expect(bot.getNotificationType()).to.equal('SILENT_PUSH')
-      })
-
-      it('should store "NO_PUSH" in notificationType if passed as an option', function () {
-        let options = {notificationType: 'NO_PUSH'}
-        let bot = new Messenger(token, options)
-
-        expect(bot.getNotificationType()).to.equal('NO_PUSH')
+        it('should store "NO_PUSH" if passed as an option', function () {
+          let options = {notificationType: 'NO_PUSH'}
+          let bot = new Messenger(token, options)
+          expect(bot.getNotificationType()).to.equal('NO_PUSH')
+        })
       })
     })
   })
